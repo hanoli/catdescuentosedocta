8a6eec6d0af24e1226b7cb4c722fef2a3df9b3ab
hanoli <hanoli@hotmail.com>
2020-11-20 16:34:06 -0600

    Se agrego la lectura del archivo

--- src/main/java/com/hanoli/catdescuentosedocta/UploadFileService.java
+++ src/main/java/com/hanoli/catdescuentosedocta/UploadFileService.java
@@ -6,6 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,6 +18,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataParam;
 
@@ -49,6 +64,8 @@ public class UploadFileService {
         if(resp) {
         	output = "Archivo cargado con exito: " + uploadedFileLocation;	
         	
+        	ReadFile(uploadedFileLocation);
+        	
         }
         
         return Response.status(200).entity(output).build(); 
@@ -80,6 +97,77 @@ public class UploadFileService {
         return carga;
 
     } 
+    
+    
+    
+    public void ReadFile(String pathFile) {
+		
+    	DataFormatter formatter = new DataFormatter();
+		
+    	Map<String, Integer> mapNombresColumnas = new HashMap<String, Integer>();
+
+		final int filaNombresColumnas = 0;
+		
+		File archivoExcel = new File(pathFile);
+		//abrir el archivo con POI
+		Workbook workbook = null;
+		try {
+			workbook = WorkbookFactory.create(archivoExcel);
+		} catch (EncryptedDocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		//Lee la posicion de la primera hoja
+		Sheet sheet = workbook.getSheetAt(0);
+		
+		//Accede a la posicion donde estan las columnas,normalmente es 0
+		Row filaNombresColumna = sheet.getRow(0);
+		
+		List<String> columnas = new ArrayList<>();
+		
+		//Obtiene el nombre de las columnas
+	     for (int cn=filaNombresColumna.getFirstCellNum(); cn<filaNombresColumna.getLastCellNum(); cn++) { 
+          Cell c = filaNombresColumna.getCell(cn); 
+          if (c == null) { 
+           // The cell is empty 
+          } else { 
+           // Process the cell 
+        	  String contenidoCelda = formatter.formatCellValue(c);
+        	  columnas.add(contenidoCelda);
+          } 
+        } 
+		
+		filaNombresColumna.cellIterator().forEachRemaining(cell -> {
+		    String valorCelda = cell.getStringCellValue().trim();
+		    if (!valorCelda.isEmpty()) {
+		        mapNombresColumnas.put(valorCelda, cell.getColumnIndex());
+		    }
+		});
+		
+	
+		int indiceDatos = filaNombresColumnas + 1;
+		Row filaDatos = null;
+		//Recorrer todas las filas con datos
+		while ((filaDatos = sheet.getRow(indiceDatos++)) != null) {
+		    //Procesa solo las celdas en base a los "nombres" de esas columnas
+		    for (String col : columnas) {
+		       //el resultado de mapNombresColumnas.get(col) es el número de columna a leer en este caso, solo se imprime el resultado
+		       System.out.print(filaDatos.getCell(mapNombresColumnas.get(col)) + " ");
+		    }
+		    
+		    System.out.println();
+		}
+		
+	}
+    
+    
     
     
 	
